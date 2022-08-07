1cf939ffc86df61be5077a2ef1cf4461eb9847cf
iiTONELOC <anthonytropeano@me.com>
2022-08-07 13:13:30 -0400

    Created a web folder and initialized a React js project in it.
    The server has been updated to run React's build script on start to
    ensure that the build is always up to date. Tested the server and I can
    successfully access the front end by running our flask server.

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import os
 from lib.alfred.alfred import Alfred
 from flask import Flask, request, make_response, render_template, send_from_directory
 
-static = './lib/ui/web'
+static = './lib/ui/web/build/'
 static = os.path.normpath(static)
 app = Flask(__name__, static_url_path='',
             static_folder=static,
@@ -18,7 +18,13 @@ def index():
 
 def start_server():
     global BOT
+
+    # Build Front-End
+    os.system('cd lib/ui/web && npm run build && cd..')
+    # Initialize Alfred, but do not run Alfred
+    # We can still use Alfred's functionality without running it
     BOT = Alfred()
+    # start the server
     app.run(debug=True, port=5000, host='0.0.0.0')
 
 
